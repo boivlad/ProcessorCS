6f973ee5fc76bd11cbf9de0892b0715396fae83f
Vladyslav Boichenko <44983140+boivlad@users.noreply.github.com>
2019-09-27 09:19:19 +0300

    Initial commit

---
+++ DataBus.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessorCS
+{
+    class DataBus
+    {
+        private int bus;
+        public DataBus(Clock clock, Processor processor)
+        {
+            
+        }
+        public int readNumber()
+        {
+            return Int32.Parse(bus.ToString().Substring(3));
+        }
+        public int readMantis()
+        {
+            return Int32.Parse(bus.ToString().Substring(0, 1));
+        }
+        public int readOrder()
+        {
+            return Int32.Parse(bus.ToString().Substring(1,2));
+        }
+        public int readReverseCode()
+        {
+            return Int32.Parse(bus.ToString().Substring(2,3));
+        }
+        public int readZero()
+        {
+            return Int32.Parse(bus.ToString().Substring(3, 4));
+        }
+        public void write( int word)
+        {
+            this.bus = word;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessorCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock clock = new Clock();
+            Processor celeron = new Processor();
+            Bus data = new Bus(clock, celeron);
+        }
+    }
+}
